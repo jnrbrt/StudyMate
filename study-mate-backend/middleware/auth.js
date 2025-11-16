bcce27e534e02aff71d2b1478742804355dd62e3
Molnár András <bandesz@mailbox.unideb.hu>
2025-11-16 21:40:27 +0100

    authentikáció megvalósítása

--- study-mate-backend/middleware/auth.js
+++ study-mate-backend/middleware/auth.js
@@ -6,8 +6,7 @@ function auth(req, res, next) {
     if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded; // a middleware után a req.user-ben lesz a userId
+        req.user = jwt.verify(token, process.env.JWT_SECRET); // a middleware után a req.user-ben lesz a userId
         next();
     } catch (err) {
         res.status(401).json({ msg: 'Token is not valid' });
