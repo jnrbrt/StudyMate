fdaddcace6e536921eec3e40f3fcb681fcdf669a
Molnár András <bandesz@mailbox.unideb.hu>
2025-11-16 21:06:48 +0100

    route-ok megváltoztatása

--- study-mate-frontend/src/main.jsx
+++ study-mate-frontend/src/main.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
-// Oldalak importálása
-import LoginPage from './pages/LoginPage'
-import RegistrationPage from './pages/RegistrationPage'
-import MainPage from './pages/MainPage'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+
+import LoginPage from './pages/LoginPage';
+import RegistrationPage from './pages/RegistrationPage';
+import MainPage from './pages/MainPage';
+import PrivateRoute from './components/PrivateRoute';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegistrationPage />} />
+        <Route 
+          path="/home" 
+          element={
+            <PrivateRoute>
+              <MainPage />
+            </PrivateRoute>
+          } 
+        />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
