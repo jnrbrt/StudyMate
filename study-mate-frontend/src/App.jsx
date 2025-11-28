38c68e3b6ec93e57468f43a9fa6b183f5a2f8319
jnrbrt <janoskovicsnorbi@gmail.com>
2025-11-28 11:06:43 +0100

    fix: Comment removed

--- study-mate-frontend/src/App.jsx
+++ study-mate-frontend/src/App.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 function App() {
-  // A 'dark' class-t a <html> tagre kell majd tenni a dark mode-hoz
-  // Itt lehetne egy globális kontextus pl. a téma váltáshoz
   return (
     <div className="bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100 min-h-screen">
       <Outlet />
