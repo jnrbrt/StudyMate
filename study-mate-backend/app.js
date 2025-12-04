94254a34d0ceac382dbbd1b8915c7605d76caea4
Molnár András <bandesz@mailbox.unideb.hu>
2025-12-04 20:57:10 +0100

    backend dotenv elérés frissítése

--- study-mate-backend/app.js
+++ study-mate-backend/app.js
@@ -1,28 +1,23 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
 
-require('dotenv').config();
+console.log("Loaded MONGO_URI:", process.env.MONGO_URI);
 connectDB();
 
 const app = express();
 
-// Middleware
-
-// Body parser: a route-ok tudják olvasni a req.body-t
 app.use(express.json());
-
-// Cookie parser: ha később cookie-t is szeretnél használni
 app.use(cookieParser());
 
-// CORS: csak a frontend URL-jét engedélyezzük, később éles környezetben könnyen módosítható
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true
 }));
 
-// Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 app.use('/api/quiz', require('./routes/quiz'));
